7337043098575f92ba8054b650f04a9db7b0dda6
Jina Strickland <jina30@gmail.com>
2020-10-15 03:01:59 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,13 @@
+famous_cats = ["lil' bub", "grumpy cat", "maru"]
+famous_cats.sort
+
+
+
+
+=begin
+favorite_number = 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2
+10.times do
+  favorite_number = favorite_number + 1
+end
+puts "And now your favorite_number is 10 more! It is #{favorite_number}"
+=end
